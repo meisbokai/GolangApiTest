5f566ced62535132336c642dfa9aaec5ba7ad5e5
Neow Bo Kai <neow.bokai@gmail.com>
2023-12-19 12:31:50 +0800

    Change timezone for createdAt

--- user.go
+++ user.go
@@ -17,10 +17,14 @@ type User struct {
 }
 
 func NewUser(username string, email string) *User {
+	// Set Singapore Timezone
+	// TODO: Check where to initialize timezone. Shouldnt be in this constructor
+	loc, _ := time.LoadLocation("Asia/Singapore")
+
 	return &User{
 		// ID:        rand.Intn(10000), // TODO: Use postgres incremental ID generation
 		Username:  username,
 		Email:     email,
-		CreatedAt: time.Now().UTC(),
+		CreatedAt: time.Now().UTC().In(loc),
 	}
 }
